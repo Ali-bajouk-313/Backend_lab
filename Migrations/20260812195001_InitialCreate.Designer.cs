c76824398cb8482963d2f6510de6f7d233d12c97
Ali Bajouk <202210889@ua.edu.lb>
2026-08-14 16:20:13 +0300

    CodeFirst

--- Migrations/20260812195001_InitialCreate.Designer.cs
+++ Migrations/20260812195001_InitialCreate.Designer.cs
@@ -18,7 +18,7 @@ namespace WarehouseManagement.Api.Migrations
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
-#pragma warning disable 612, 618
+            #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("ProductVersion", "10.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
